33a2a317044d867426edca09ef673e3e5ca73e4a
VulgarBear <lucas@shisify.com>
2022-08-16 09:53:34 -0400

    added docker install script

---
+++ docker/install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#
+# Installs docker.
+
+info 'Installing docker...'
+
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+echo ' '
+info 'Verifying Pulling from Docker Repo...'
+apt-cache policy docker-ce
+sleep 3
+
+sudo apt install docker-ce
+
+echo ' '
+info 'Verifying Docker is installed...'
+sudo systemctl status docker
+sleep 1
+
+echo ' '
+info 'Adding user to Docker group...'
+sudo usermod -aG docker vulgarbear
+
+echo ' '
+success 'Docker install compelted...'
--- script/install.sh
+++ script/install.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 #
-# bootstrap installs things.
+# Run all dotfiles installers.
+
+set -e
+
+cd "$(dirname $0)"/..
+
+# find the installers and run them iteratively
+find . -name install.sh | while read installer ; do sh -c "${installer}" ; done
