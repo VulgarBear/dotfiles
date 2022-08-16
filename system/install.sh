01c3ce1707daf691fe3f5f8a4a46265d9f8467cb
VulgarBear <lucas@shisify.com>
2022-08-16 14:05:44 -0400

    added system installer

---
+++ system/install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+#
+# Installs systems tools such as exa and etc.
+# Setup
+
+# Setup
+set -e
+
+echo ''
+
+source "$HOME/.dotfiles/utils/logging_utils.sh"
+
+#Script Start
+echo ''
+info "Beginning system tool installations..."
+sleep 1
+
+# Prereqs
+echo ''
+info "Installing Prereqs..."
+sleep 1
+sudo apt install unzip -y
+
+## Installing Rust Compiler
+echo ''
+info "Installing Prereqs: Rust Compiler"
+user "When prompted select first option..."
+sleep 1
+curl https://sh.rustup.rs -sSf | sh
+
+## Exa Install and config
+info "Installing Exa..."
+wget -c https://github.com/ogham/exa/releases/download/v0.8.0/exa-linux-x86_64-0.8.0.zip
+unzip exa-linux-x86_64-0.8.0.zip
+sudo mv exa-linux-x86_64 /usr/local/bin/exa
+
+# Script End
+echo ''
+success "System Installation Completed..."
