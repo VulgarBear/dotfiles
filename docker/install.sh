accf3187a9cdf7275270177110ec0809316cf17b
VulgarBear <lucas@shisify.com>
2022-08-16 14:17:48 -0400

    remote apt update from docker install

--- docker/install.sh
+++ docker/install.sh
@@ -13,7 +13,6 @@ source "$HOME/.dotfiles/utils/logging_utils.sh"
 #Script Start
 info 'Installing docker...'
 
-sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
